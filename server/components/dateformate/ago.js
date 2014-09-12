a50eccbd14e861810aa5f42a010b4aa094f0b145
zhangzimeng01@baidu.com <zhangzimeng01>
2014-09-12 13:38:13 +0800

    update

--- server/components/dateformate/ago.js
+++ server/components/dateformate/ago.js
@@ -2,12 +2,12 @@ var dateFormat = require('dateformat');
 
 function getDateDiff(date) {
 	var now = new Date();
-	var formate = "yyyy-MM-dd HH:mm:ss";
+	var formate = "yyyy-mm-dd HH:MM";
 	if (now.getDate() == date.getDate()) {
 
 		formate = "HH:MM"
 	}else if(now.getFullYear() == date.getFullYear()) {
-		formate = "mm-dd HH:MM:ss";
+		formate = "mm-dd HH:MM";
 	}
 	return dateFormat(date,formate);
 }
