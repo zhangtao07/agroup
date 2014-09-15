4973718d55a2bdaaaffad33a547e88fd6e5549f3
zhang-ning <hunter.dding@gmail.com>
2014-09-15 10:50:15 +0800

    合并编辑器代码

--- server/routes.js
+++ server/routes.js
@@ -17,6 +17,8 @@ module.exports = function(app) {
 
   app.use('/auth', require('./auth'));
 
+  app.use('/editor', require('./editor'))
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
