c95d2b0d45d9ef291bb4c606c376b27668dd147c
wuduoyi <duoyi.wu@gmail.com>
2014-09-16 12:55:37 +0800

    修复配置文件的错误

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,48 +3,13 @@
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server port
-  port: process.env.OPENSHIFT_NODEJS_PORT ||
-    process.env.PORT ||
-    8999,
-
-  // MongoDB connection options
-  mongo: {
-    uri: process.env.MONGOLAB_URI ||
-      process.env.MONGOHQ_URL ||
-      process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME ||
-      'mongodb://localhost/agroup'
-  },
-  my sql
-:
-{
-  protocol: "mysql",
-    query
-:
-  {
-    pool: true
+  mysql: {
+    protocol: "mysql",
+    query: { pool: true },
+    host: "127.0.0.1",
+    database: "agroup",
+    user: "root",
+    password: "end",
+    port: 3306
   }
-,
-  host: "127.0.0.1",
-    database
-:
-  "agroup",
-    user
-:
-  "root",
-    password
-:
-  "end"
-}
-,
-
-auth: 'uuap',
-
-  upload_temp_dir
-:
-"/home/agroup/files/upload/.tmp",
-  upload_dir
-:
-"/home/agroup/files/upload/"
-}
-;
+};
