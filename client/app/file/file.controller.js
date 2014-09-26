973a761ef8db651fcacf909007fd885f063a028a
zhang-ning <hunter.dding@gmail.com>
2014-09-26 19:35:42 +0800

    添加文件拖拽上传

--- client/app/file/file.controller.js
+++ client/app/file/file.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('agroupApp')
-  .controller('FileCtrl', function($scope,$stateParams,$http) {
+  .controller('FileCtrl', function($scope, $stateParams, $http) {
     $scope.message = 'Hello';
 
     var level = [{
@@ -9,20 +9,21 @@ angular.module('agroupApp')
     }];
     var db;
 
-    function getChild(lel){
-      return db.filter(function(d){
-        if(d.parent_id === lel){
+    function getChild(lel) {
+      return db.filter(function(d) {
+        if (d.parent_id === lel) {
           return d;
         }
       });
     }
 
     init();
-    function init(){
+
+    function init() {
       var group = $stateParams.group;
-      $http.get('api/files/'+group).success(function(data,status) {
+      $http.get('api/files/' + group).success(function(data, status) {
         db = data;
-        window.db= db;
+        window.db = db;
         level[0].files = level[0].files.concat(getChild(0));
         $scope.level = level;
       })
@@ -30,50 +31,50 @@ angular.module('agroupApp')
 
     window.level = level;
 
-    function selectItem(folder,item) {
-      if(folder.selectedItem){
+    function selectItem(folder, item) {
+      if (folder.selectedItem) {
         folder.selectedItem.selected = false;
       }
       item.selected = true;
       folder.selectedItem = item;
 
       var index = level.indexOf(folder);
-      var nextLevel = level[index+1] = level[index+1] || {};
-      nextLevel.files  = getChild(item.id);
+      var nextLevel = level[index + 1] = level[index + 1] || {};
+      nextLevel.files = getChild(item.id);
 
       //if(item.selected){ return }
       //folder.selectedItem = item;
       //nextFolder.selectedItem = item;
 
-      level.splice(index+2,level.length-index-2);
+      level.splice(index + 2, level.length - index - 2);
 
       //var selectedChild = _.filter(nextFolder.files,function(d){
-        //if(d.selected){
-          //return d;
-        //}
+      //if(d.selected){
+      //return d;
+      //}
       //});
 
       //if(selectedChild.length){
-        //selectItem(nextFolder,selectedChild[0]);
+      //selectItem(nextFolder,selectedChild[0]);
       //}
     }
 
-    function clearSelect(folder){
-      if(folder.selectedItem){
+    function clearSelect(folder) {
+      if (folder.selectedItem) {
         folder.selectedItem.selected = false;
       }
     }
 
-    function deleteItem(folder,item) {
+    function deleteItem(folder, item) {
       var i = folder.files.indexOf(item);
-      folder.files.splice(i,1);
+      folder.files.splice(i, 1);
       var j = level.indexOf(folder);
-      if(item.selected){
-        level.splice(j+1,level.length-j-1);
+      if (item.selected) {
+        level.splice(j + 1, level.length - j - 1);
       }
 
-      if(!folder.files.length){
-        level.splice(j,1);
+      if (!folder.files.length) {
+        level.splice(j, 1);
       }
     }
 
@@ -81,11 +82,11 @@ angular.module('agroupApp')
       var files = level[index].files = level[index].files || [];
 
       var data = {
-        name: 'level'+ index +'-folder' + level[index].files.length,
-        parent_id: index > 0 ? level[index-1].selectedItem.id : 0,
+        name: 'level' + index + '-folder' + level[index].files.length,
+        parent_id: index > 0 ? level[index - 1].selectedItem.id : 0,
         group_id: $stateParams.group
       };
-      $http.post('api/files/',data).success(function(d,status){
+      $http.post('api/files/', data).success(function(d, status) {
         db.push(d);
         //var files = level[index].files = level[index].files || [];
         level[index].files.push(d);
@@ -98,4 +99,22 @@ angular.module('agroupApp')
     $scope.deleteItem = deleteItem;
     $scope.createFolder = createFolder;
 
+
+    var panel = $scope.uploadpanel = {}
+
+    function sendFile(file) {
+      var groupId = $stateParams.group;
+      panel.addFile(file, function(file, send) {
+        var formData = new FormData();
+        formData.append('groupId', groupId);
+        formData.append('file', file);
+        send('api/message/upload', formData);
+
+      });
+    }
+    $scope.onDrop = function(files) {
+      files.forEach(function(file) {
+        sendFile(file);
+      });
+    };
   });
