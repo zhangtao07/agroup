7ceb3b436fe5be60c3202a96acacd2688bd7967c
zhang-ning <hunter.dding@gmail.com>
2014-10-11 13:26:39 +0800

    max upload file size

--- server/config/express.js
+++ server/config/express.js
@@ -24,7 +24,7 @@ module.exports = function(app) {
   app.engine('html', require('ejs').renderFile);
   app.set('view engine', 'html');
   app.use(compression());
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.urlencoded({ limit: '50mb' ,extended: false }));
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
