af74364458cbb8e5c4b1a0d9ff5c3b070034b207
baidu <baidu@baidudeMacBook-Pro.local>
2014-11-21 11:01:37 +0800

    update config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -13,5 +13,11 @@ module.exports = {
     view: '/op/view.aspx?src=',
     embed: '/op/embed.aspx?src='
   },
-  hostname:'agroup.baidu.com'
+  hostname:'agroup.baidu.com',
+  service:{
+    host : 'cp01-ra-045.cp01.baidu.com',
+    port : '8087'
+    //host : 'localhost',
+    //port: 8080
+  }
 };
